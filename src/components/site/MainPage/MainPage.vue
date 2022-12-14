da1eae38d0d2f162d042fb4161d893cb7a6b2321
OGarmonenko <olga.garmonenko@list.ru>
2022-12-14 13:47:40 +0300

    1 - inited project

---
+++ src/components/site/MainPage/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper-main">Main Page</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "MainPage",
+});
+</script>
+
+<style scoped>
+.wrapper-main {
+  padding: 40px 20px;
+}
+</style>
