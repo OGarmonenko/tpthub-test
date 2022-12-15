73bcbf95f0b071893da396288f16359e820df579
OGarmonenko <olga.garmonenko@list.ru>
2022-12-15 12:29:49 +0300

    12 - developed CustonSelect.vue and Pagination.vue

---
+++ src/components/shared/CustomSelect.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper-select">
+    <label class="label-select">{{ title }}</label>
+    <select class="custom-select" @change="changeOption">
+      <option v-for="(opt, i) in options" :key="i" :value="opt">
+        {{ opt }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "CustomSelect",
+  props: {
+    modelValue: {
+      type: String,
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+    title: String,
+  },
+  methods: {
+    changeOption(e) {
+      this.$emit("change", e.target.value);
+    },
+  },
+});
+</script>
+
+<style scoped>
+.wrapper-select {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+.custom-select {
+  min-width: 40px;
+  padding: 0 10px;
+  border: 1px solid teal;
+  height: 50px;
+  outline: none;
+  font-size: 15px;
+}
+</style>
---
+++ src/components/shared/Pagination.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="page-wrapper">
+    <div
+      v-for="pageNumber in totalPage"
+      :key="pageNumber"
+      class="page"
+      :class="{ 'current-page-active': currentPage === pageNumber }"
+      @click="handleClick(pageNumber)"
+    >
+      {{ pageNumber }}
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Pagination",
+  components: {},
+  props: {
+    totalPage: {
+      type: Number,
+    },
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    handleClick(item: number) {
+      this.$emit("onClick", item);
+    },
+  },
+});
+</script>
+
+<style scoped>
+.page-wrapper {
+  display: flex;
+  margin-top: 15px;
+  width: 300px;
+  gap: 10px;
+}
+.page {
+  border: 1px solid teal;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.current-page-active {
+  border: 3px solid teal;
+  padding: 10px;
+}
+</style>
